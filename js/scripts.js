eaba8edbb785c4b3bd4e1b0c23e541796902b57f
Eric Liu <ericliu@Eric-Lius-MacBook-Pro.local>
2016-08-15 15:37:48 -0400

    add Alarm

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,21 @@
 function AlarmClock(container) {
   'use strict'
 
+  var alarm, display;
+
   var state = {
-    alarm: false,
+    alarmSet: false,
     date: new Date(),
     settings: {
       twelveHours: true
     }
   }
 
-  var display;
-
-  function checkAlarms() {
-    if (state.alarm) {
-
+  function checkAlarm() {
+    console.log('checkAlarm')
+    console.log(state.alarmSet)
+    if (state.alarmSet && alarm.check(state.date)) {
+      alert('alarm!')
     }
   }
 
@@ -25,15 +27,16 @@ function AlarmClock(container) {
     var settingsEl = document.createElement('div')
 
     el.className = 'alarm-clock alarm-clock--loading'
-    alarmsEl.className = 'alarm-clock__alarms'
+    alarmsEl.className = 'alarm-clock__alarm'
     settingsEl.className = 'alarm-clock__settings'
 
     container.insertBefore(el, null)
-    el.insertBefore(alarmsEl, null)
     el.insertBefore(settingsEl, null)
+    el.insertBefore(alarmsEl, null)
 
     display = new Display(el, state.date, state.settings)
 
+    alarm = new Alarm(alarmsEl, updateAlarmSet)
     new Settings(settingsEl, updateSettings)
 
     setTimeout(function() {
@@ -51,7 +54,11 @@ function AlarmClock(container) {
     state.date = new Date()
 
     updateDisplay()
-    checkAlarms()
+    checkAlarm()
+  }
+
+  function updateAlarmSet(newState) {
+    state.alarmSet = newState
   }
 
   function updateDisplay() {
@@ -69,20 +76,153 @@ function AlarmClock(container) {
 }
 
 // Alarms ----------------------------------------------------------------------
-function Alarms() {
+function Alarm(container, updateAlarmSet) {
+  'use strict'
 
+  var state = {
+    hours: 1,
+    minutes: 0,
+    on: false,
+    period: 'AM'
+  }
+
+  function check(date) {
+    var h, m
+
+    console.log('check')
+
+    if (state.on) {
+      h = state.period === 'AM' ? parseInt(state.hours) : parseInt(state.hours) + 12
+      m = parseInt(state.minutes)
+
+      if (date.getHours() === h && date.getMinutes() === m && date.getSeconds() === 0) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  function render() {
+    var el = document.createElement('div')
+    var iconEl = document.createElement('div')
+    var optionsEl = document.createElement('div')
+    var labelEl = document.createElement('label')
+    var hoursOptionEl = document.createElement('select')
+    var minutesOptionEl = document.createElement('select')
+    var periodOptionEl = document.createElement('select')
+    var checkboxEl = document.createElement('input')
+
+    checkboxEl.setAttribute('type', 'checkbox')
+
+    el.className = 'alarm'
+    iconEl.className = 'alarm__icon fa fa-bell-slash'
+    optionsEl.className = 'alarm__options'
+    labelEl.className = 'alarm__label'
+    hoursOptionEl.className = 'alarm__select'
+    minutesOptionEl.className = 'alarm__select'
+    periodOptionEl.className = 'alarm__select'
+    checkboxEl.className = 'alarm__checkbox'
+
+    iconEl.onclick = toggle.bind(el)
+    hoursOptionEl.onchange = update.bind(hoursOptionEl, 'hours')
+    minutesOptionEl.onchange = update.bind(minutesOptionEl, 'minutes')
+    periodOptionEl.onchange = update.bind(periodOptionEl, 'period')
+    checkboxEl.onclick = update.bind(checkboxEl, 'on', iconEl)
+
+    labelEl.innerHTML = 'Set alarm'
+
+    for (var i = 1; i < 13; i++) {
+      var opt = document.createElement('option')
+      opt.value = i
+      opt.innerHTML = i
+      hoursOptionEl.appendChild(opt)
+    }
+
+    for (var i = 0; i < 59; i++) {
+      var opt = document.createElement('option')
+      opt.value = i
+      opt.innerHTML = i < 10 ? '0' + i : i
+      minutesOptionEl.appendChild(opt)
+    }
+
+    ;(function() {
+      var optAM = document.createElement('option');
+      var optPM = document.createElement('option');
+
+      optAM.value = 'AM'
+      optAM.innerHTML = 'AM'
+
+      optPM.value = 'PM'
+      optPM.innerHTML = 'PM'
+
+      periodOptionEl.appendChild(optAM)
+      periodOptionEl.appendChild(optPM)
+    })()
+
+    container.insertBefore(el, null)
+    el.insertBefore(iconEl, null)
+    el.insertBefore(optionsEl, null)
+    optionsEl.insertBefore(labelEl, null)
+    optionsEl.insertBefore(hoursOptionEl, null)
+    optionsEl.insertBefore(minutesOptionEl, null)
+    optionsEl.insertBefore(periodOptionEl, null)
+    optionsEl.insertBefore(checkboxEl, null)
+  }
+
+  function toggle() {
+    this.classList.toggle('alarm--active')
+  }
+
+  function toggleIcon(iconEl) {
+    if (state.on) {
+      iconEl.classList.remove('fa-bell-slash')
+      iconEl.classList.add('fa-bell', 'alarm__icon--active')
+
+      return
+    }
+
+    iconEl.classList.remove('fa-bell', 'alarm__icon--active')
+    iconEl.classList.add('fa-bell-slash')
+  }
+
+  function update(type, iconEl) {
+    switch (type) {
+      case 'hours':
+        state.hours = this.value
+        return
+      case 'minutes':
+        state.minutes = this.value
+        return
+      case 'period':
+        state.period = this.value
+        return
+      case 'on':
+        var on = this.checked
+        state.on = on
+        updateAlarmSet(on)
+        toggleIcon(iconEl)
+        return
+    }
+  }
+
+  render()
+
+  return {
+    check: check
+  }
 }
 
 // Display ---------------------------------------------------------------------
 function Display(container, date, settings) {
   'use strict'
 
+  var dateEl, hoursEl, minutesEl, periodEl, secondsEl
+
   var state = {
     date: date,
     settings: settings
   }
-
-  var dateEl, hoursEl, minutesEl, periodEl, secondsEl
 
   function getDateToStr() {
     return state.date.toDateString()
@@ -177,6 +317,10 @@ function Display(container, date, settings) {
 function Settings(container, updateSettings) {
   'use strict'
 
+  function hide() {
+    this.classList.remove('settings--active')
+  }
+
   function render() {
     var el = document.createElement('div')
     var iconEl = document.createElement('div')
@@ -213,6 +357,10 @@ function Settings(container, updateSettings) {
   }
 
   render()
+
+  return {
+    hide: hide
+  }
 }
 
 // -----------------------------------------------------------------------------
